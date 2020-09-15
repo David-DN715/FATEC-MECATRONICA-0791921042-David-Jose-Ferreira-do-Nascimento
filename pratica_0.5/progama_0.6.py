650d13659ce8486ed02d9a6103b4bb80a73bf215
David-DN715 <70581471+David-DN715@users.noreply.github.com>
2020-09-15 08:39:30 -0300

    Create progama_0.6.py

---
+++ pratica_0.5/progama_0.6.py
@@ -0,0 +1,12 @@
+#Pede ao usuario para digitar a senha
+#Criar uma variavel para a senha
+senha_secreta ="123456"
+
+#Pede para o usuario digitar a senha
+senha = input("digite a senha")
+
+ #verificar se a senha está correta
+if senha == senha_secreta:
+  print ("Bem vindo Hackerman")
+else:
+  print("Acesso negado")
