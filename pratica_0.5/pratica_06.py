f9906f80eb68bdf4ac2c6704f32c802757b6314f
David-DN715 <70581471+David-DN715@users.noreply.github.com>
2020-09-15 08:53:30 -0300

    Create pratica_06.py

---
+++ pratica_0.5/pratica_06.py
@@ -0,0 +1,13 @@
+#codigo verificador de numeros par e impar
+#operadores da divisão(/) e resto da divisão(%)
+
+numero = int(input("digite um numero"))
+ #calcular o valor do resto 
+resto = numero % 2
+#verificar o valor do resto
+if resto == 0:
+  print ("esse numero é par")
+else:
+  print("esse numero é impar")
+  
+
