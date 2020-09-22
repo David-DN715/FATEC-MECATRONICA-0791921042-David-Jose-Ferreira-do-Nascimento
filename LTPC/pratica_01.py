52aa4fac5d88274aefb63363fa079e5968e80165
David-DN715 <70581471+David-DN715@users.noreply.github.com>
2020-09-22 08:31:09 -0300

    Create pratica_01.py

---
+++ LTPC/pratica_01.py
@@ -0,0 +1,8 @@
+#este codigo mostra o menos valor
+if valor1 =< valor2 and valor1 =< valor3:
+ menor = valor1
+ else valor2 =< valor1 and valor2 =< valor3:
+ menor = valor2
+ else valor3 =< valor1 and valor3 =< valor2:
+ menor = valor3
+print ("o menor valor é")
