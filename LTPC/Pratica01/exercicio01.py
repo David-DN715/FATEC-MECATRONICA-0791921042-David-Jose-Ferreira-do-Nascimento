0bd87a70275866fa16032854bcc2fa3a698dbe7d
David-DN715 <70581471+David-DN715@users.noreply.github.com>
2020-09-22 08:33:43 -0300

    Create exercicio01.py

---
+++ LTPC/Pratica01/exercicio01.py
@@ -0,0 +1,8 @@
+
+if valor1 =< valor2 and valor1 =< valor3:
+ menor = valor1
+ else valor2 =< valor1 and valor2 =< valor3:
+ menor = valor2
+ else valor3 =< valor1 and valor3 =< valor2:
+ menor = valor3
+print ("o menor valor é")
