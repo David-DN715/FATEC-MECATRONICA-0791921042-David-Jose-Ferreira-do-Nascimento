fdca8dfd0fe07c1e74419902cffa22f71b4a8ba8
David-DN715 <70581471+David-DN715@users.noreply.github.com>
2020-09-29 09:29:32 -0300

    Create pratica08.py

---
+++ LTPC/pratica06/pratica08.py
@@ -0,0 +1,12 @@
+soma = 0
+contador = 0
+
+#o while aqui está fazende uma estrutura para repetir uma condição inposta a ele.
+while contador < 3:
+  numero = int(input("Numero:"))
+  soma = soma + numero
+  contador = contador + 1
+
+  
+media = soma /3
+print("o valor medio:", media)
