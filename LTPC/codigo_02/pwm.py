dffb36eb19c6e50776e17f134c1697f00d250498
David-DN715 <70581471+David-DN715@users.noreply.github.com>
2020-10-06 13:22:10 -0300

    Create pwm.py

---
+++ LTPC/codigo_02/pwm.py
@@ -0,0 +1,20 @@
+#link usado para o codigo do exercicio abaixo para compreensão do PWM
+#V_out: Tensão de Saída
+#T_ON: Tempo em nível alto
+#T: Período total do sinal
+#V_ON: Tensão em nível alto
+continuar ="sim"
+
+while continuar == "sim":
+ T=float(input("digite o periodo total"))
+ V_on =float(input("digite a tensão no nivel alto:"))
+ T_on = float(input("digite o tempo que se manteve ativo :"))
+ continuar = input("quer continuar?")
+ if T == 0:
+    T=float(input("redigite o periodo total:"))
+ if V_on == 0:
+    V_on = int(input("redigite a tensão no nivel alto:"))
+ else:
+    V_out = T_on/T*V_on
+
+print(V_out)
