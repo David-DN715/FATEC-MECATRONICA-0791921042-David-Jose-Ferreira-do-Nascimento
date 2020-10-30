dc5777af08d7856c6897ef4c797a59ac4ae01686
David-DN715 <70581471+David-DN715@users.noreply.github.com>
2020-10-30 17:50:39 -0300

    Create projeto03.py

---
+++ projeto_LTPC01/projeto/projeto03.py
@@ -0,0 +1,16 @@
+#terceira versão do codigo acima 
+numero_da_conta= 0
+contador_de_contas =0
+
+#aqui se cria usuario de conta e adiciona no contador_de_contas 
+while contador_de_contas == 0:
+ usuario = input("crie aqui seu usuario:\n")
+ senha = int(input("crie aqui senha:\n"))
+ contador_de_contas +=1
+ if numero_da_conta <= 999:
+   numero_da_conta == contador_de_contas
+   break
+numero_da_conta += 1
+contador_de_contas +=1
+
+print("parabens", usuario, "o numero da sua conta é", numero_da_conta)
