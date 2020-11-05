c079ffd86e38021fa68ace3fcfb7acd5003fcc25
David-DN715 <70581471+David-DN715@users.noreply.github.com>
2020-11-05 14:07:00 -0300

    Create projeto_tranferidor_de_saldo.py

---
+++ projeto_LTPC01/projeto/projeto_tranferidor_de_saldo.py
@@ -0,0 +1,14 @@
+ #A principio o codigo abaixo é uma versão de elo de ferro do transferidor de Saldo que irá conter no projeto!
+ 
+ #aqui ficara o edtudo de valores de ransferencia:
+dinheiro_em_conta1= 1000
+dinheiro_em_conta2 = 250
+
+#Aqui em Saldo novo ficaria sendo como uma transferencia de conta
+saldo_novo = dinheiro_em_conta1 + dinheiro_em_conta2
+
+#Aqui ficaria o novo saldo da conta1 apos a transferencia 
+dinheiro_em_conta1_00 = dinheiro_em_conta1 - dinheiro_em_conta2
+
+#esse print foi usado para testar as variantes acima!
+print (saldo_novo) 
